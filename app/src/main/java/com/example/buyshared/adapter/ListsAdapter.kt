3efd1a4c1b94ef909a46d10bb23b986abea675ea
Raul Remedios Rosado <62999152+raulrosado@users.noreply.github.com>
2023-05-20 20:10:43 -0500

    Add: load avatars from db to show

--- app/src/main/java/com/example/buyshared/adapter/ListsAdapter.kt
+++ app/src/main/java/com/example/buyshared/adapter/ListsAdapter.kt
@@ -6,12 +6,17 @@ import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.buyshared.R
 import com.example.buyshared.data.model.ListsEntity
+import com.example.buyshared.ui.MainViewModel
 
-class ListsAdapter(private val lists: List<ListsEntity>,private val requireActivity: FragmentActivity) : RecyclerView.Adapter<ListsViewHolder>() {
+class ListsAdapter(
+    private val lists: List<ListsEntity>,
+    private val requireActivity: FragmentActivity,
+    private val mainViewModel: MainViewModel
+) : RecyclerView.Adapter<ListsViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListsViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        return ListsViewHolder(layoutInflater.inflate(R.layout.item_list,parent,false),parent,requireActivity)
+        return ListsViewHolder(layoutInflater.inflate(R.layout.item_list,parent,false),parent,requireActivity,mainViewModel)
     }
 
     override fun getItemCount(): Int = lists.size
--- app/src/main/java/com/example/buyshared/adapter/ListsViewHolder.kt
+++ app/src/main/java/com/example/buyshared/adapter/ListsViewHolder.kt
@@ -5,15 +5,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentTransaction
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.buyshared.R
+import com.example.buyshared.data.model.AvatarsEntity
 import com.example.buyshared.data.model.ListsEntity
 import com.example.buyshared.databinding.ItemListBinding
 import com.example.buyshared.ui.Activity.ReplaceFragment
 import com.example.buyshared.ui.Activity.TinyDB
 import com.example.buyshared.ui.Fragment.Inside.DetailFragment
+import com.example.buyshared.ui.MainViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class ListsViewHolder(private val view: View, private val parent: ViewGroup,private val requireActivity: FragmentActivity) :
+class ListsViewHolder(
+    private val view: View,
+    private val parent: ViewGroup,
+    private val requireActivity: FragmentActivity,
+    private val mainViewModel: MainViewModel
+) :
     RecyclerView.ViewHolder(view) {
     val binding = ItemListBinding.bind(view)
     val replaceFragment = ReplaceFragment()
@@ -24,6 +35,16 @@ class ListsViewHolder(private val view: View, private val parent: ViewGroup,priv
         binding.itemListNombre.text = listModel.nombre
         binding.itemCantArticulos.text = listModel.cant.toString()
         fragmentTransaction = requireActivity.supportFragmentManager.beginTransaction()
+
+        val recyclerAvatarsList = binding.recyclerAvatarItemList
+        recyclerAvatarsList.layoutManager = LinearLayoutManager(requireActivity, LinearLayoutManager.HORIZONTAL, false)
+
+        val avatarsList = mainViewModel.loadAvatarDBByIdList(listModel._id)
+        Log.v("buySharedLog", "cantidad avatar"+avatarsList.size)
+//        CoroutineScope(Dispatchers.IO).launch {
+//           val alllist = mainViewModel.getAllAvatarsDBList()
+//           Log.v("buySharedLog", "cantidad avatar"+alllist.size)
+//        }
 
         binding.layoutList.setOnClickListener {
             Log.v("buysharedLog", "lista seleccionada:" + listModel.nombre)
